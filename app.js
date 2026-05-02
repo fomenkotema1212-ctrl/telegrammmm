d0e3454e590cd4f00fda8250719aeba189432f45
fomenkotema1212-ctrl <fomenkotema1212@gmail.com>
2026-05-02 21:54:29 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+app.init({
+  name: "TGMessenger",
+  packageId: "com.tgmessenger.app",
+  version: "1.0.0",
+  permissions: ["INTERNET", "CAMERA", "RECORD_AUDIO"],
+  assets: "./assets",
+  startUrl: "index.html"
+});
---
+++ nitron.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: "TGMessenger",
+  packageId: "com.tgmessenger.app",
+  version: "1.0.0",
+  permissions: ["INTERNET", "CAMERA", "RECORD_AUDIO"]
+};
